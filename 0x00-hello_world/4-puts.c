2faf5627c4374e9a7a79b3f90952243f3416ec86
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-02 14:55:21 -0700

    C program: using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print using puts
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
