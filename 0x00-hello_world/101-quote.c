0606623fd0bc9163bbc3b839550c115c55323cc8
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-02 20:43:01 -0700

    C program: print to stdout using write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print using write
+ *
+ * DESCRIPTION: print without using printf or puts
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
