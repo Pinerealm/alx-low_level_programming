e2dd5d2e5c679a160a6068b565ed42be2e9ca0e9
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-03 20:29:10 -0700

    C program: checking conditions on the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,27 +3,34 @@
 #include <stdio.h>
 
 /**
- * main - displays if the last digit of a random no fulfills certain condition
+ * main - displays strings that are the result of checking the last digit
+ *        of a number
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 
-        int l_digit = n % 10;
-        if (l_digit > 5)
-                printf("Last digit of %d is %d and is greater than 5\n", n, \
-            l_digit);
-        else if (l_digit == 0)
-                printf("Last digit of %d is %d and is 0\n", n, l_digit);
-        else
-                printf("Last digit of %d is %d and is less than 6 and not 0\n",\
-                n, l_digit);
-        return (0);
+	int l_digit = n % 10;
+
+	if (l_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_digit);
+	}
+	else if (l_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_digit);
+	}
+
+	return (0);
 }
