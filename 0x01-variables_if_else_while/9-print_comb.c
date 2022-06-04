27eeb5790102a9091cd4abff77208943497e1020
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-03 22:15:08 -0700

    C program: prints comma- abd space-seperated combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
