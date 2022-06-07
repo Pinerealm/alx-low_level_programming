5f0fd1e53a155e8af2a67ba68ff6ebf2800edb1b
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-07 16:24:24 -0700

    C program: print possible combinations of two 2-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two 2-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (i = 0; i < 100; i++)
+		putchar('\n');
+
+	return (0);
+}
