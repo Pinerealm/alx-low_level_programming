0a6c06e008f7f6830101b3b699e0b9bdfd9f1efb
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-07 16:58:29 -0700

    C function: prints 10 lines of the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints the alphabet in lowercase followed
+ *                  by a new line 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(j);
+	}
+}
