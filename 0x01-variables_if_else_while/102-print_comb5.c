8dadf8efab5887d4f51b79e14398cb7c309098eb
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-07 16:36:22 -0700

    Updated code for 102-print.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,8 +9,26 @@
 
 int main(void)
 {
-	for (i = 0; i < 100; i++)
-		putchar('\n');
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 98 && n2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
 	return (0);
-}
+}	
