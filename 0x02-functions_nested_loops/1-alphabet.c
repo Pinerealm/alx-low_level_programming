6934174b8d98e35286fc7ec1109aba95efb6d244
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-07 13:17:39 -0700

    C program: print the alphabet in lowercase using -putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lower case, followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+
+	return (0);
+}
