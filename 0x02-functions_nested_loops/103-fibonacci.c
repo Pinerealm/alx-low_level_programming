4d8ab6bdb81045e2a3223787d9830b847c2822ac
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-08 18:58:12 -0700

    C program: sum of even Fibonacci numbers below 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even Fibonacci numbers less than 4,000,000
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 1;
+	int j = 2;
+	int sum = 0;
+	int next = 0;
+
+	while (next < 4000000)
+	{
+		next = i + j;
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+		i = j;
+		j = sum;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
