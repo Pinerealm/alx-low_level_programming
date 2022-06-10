6be355b5209b44ce1460ecbae1a7e7811aff051a
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-09 22:28:39 -0700

    C function: print 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9, except 2 and 4 followed by a
+ *                 new line
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isdigit(int c);
 int mul(int, int);
 void print_numbers(void);
 void _putchar(char c);
+void print_most_numbers(void);
