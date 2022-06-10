cb7bcd37dce1550590450a2da94c99c409bc043d
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-09 22:57:07 -0700

    C function: print 0-14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 - 14 followed by a new line 
+ * 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+
+			j++;
+		}
+		_putchar('\n');
+
+		i++;
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int mul(int, int);
 void print_numbers(void);
 void _putchar(char c);
 void print_most_numbers(void);
+void more_numbers(void);
