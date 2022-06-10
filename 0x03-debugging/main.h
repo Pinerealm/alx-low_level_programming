4640c92d2f0f77e999424aaad2bb29cec19828c8
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-09 20:55:25 -0700

    Debugging: print_remaining_days fxn

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int );
+int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
