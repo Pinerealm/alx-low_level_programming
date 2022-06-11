3f494e15219fc8822796d567dd28f7b8a2258e4f
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-11 07:21:26 -0700

    Code update for 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,22 @@
 
 int main(void)
 {
-	int i;
+	int i, j = 1;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d", j)
+	for (i = 2; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf(" Buzz");
 
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	printf("\n");
 
