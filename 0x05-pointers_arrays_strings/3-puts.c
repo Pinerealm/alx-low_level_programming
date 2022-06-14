bdcead3e39b4024b15456c9117577a7d58eb87ce
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-13 19:39:08 -0700

    C function: prints a string to stdout plus newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: pointer to a string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int cnt = 0;
+
+	while (str[cnt] != '\0')
+	{
+		_putchar(str[cnt]);
+		cnt++;
+	}
+	_putchar('\n');
+}
