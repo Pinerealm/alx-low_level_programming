2a841b07c77326651036eed86e3b50b617d0c398
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-16 14:56:03 -0700

    Updated _strcpy code

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,41 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
+int _strlen(char *s);
 
 /**
 * _strcpy - Copy paste string
-* @dest: destination
-* @src: source
+*
+* @dest: pointer to copied string
+* @src: pointer to string to be copied
+*
 * Return: dest
 */
 
 char *_strcpy(char *dest, char *src)
 {
-	strcpy(dest, src);
+	int i, len = _strlen(src);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
 
 	return (dest);
+}
+
+/**
+* _strlen - get string length
+* @s: pointer to string
+*
+* Return: dest
+*/
+
+int _strlen(char *s)
+{
+	int cnt = 0;
+
+	while (*(s + cnt) != '\0')
+		cnt++;
+
+	return (cnt);
 }
