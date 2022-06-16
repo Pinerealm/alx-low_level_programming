f098d047f31bdfe27c2a64fa4ec75303d9b6e23f
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-16 16:44:19 -0700

    C function: _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: pointer to string to copy to
+ * @src: pointer to string to be copies
+ * @n: number of bytes / characters to copy
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
+
+/**
+* _strlen - get string length
+* @s: pointer to string
+*
+* Return: dest
+*/
+
+int _strlen(char *s)
+{
+	int cnt = 0;
+
+	while (*(s + cnt) != '\0')
+		cnt++;
+
+	return (cnt);
+}
