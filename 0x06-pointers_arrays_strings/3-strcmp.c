dceb6a0f2825c6566c3994475fd0754c923eb3ad
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-16 20:53:40 -0700

    C function: compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: 0 if equal, a negative no if less than, a positive no otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, len_s1 = _strlen(s1);
+	int len_s2 = _strlen(s2);
+	int u_len = (len_s1 < len_s2) ? len_s1 : len_s2;
+
+	for (i = 0; i < u_len; i++)
+	{
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+/**
+* _strlen - get string length
+* @s: pointer to string
+*
+* Return: dest
+*/
+
+int _strlen(char *s)
+{
+	int cnt = 0;
+
+	while (*(s + cnt) != '\0')
+		cnt++;
+
+	return (cnt);
+}
