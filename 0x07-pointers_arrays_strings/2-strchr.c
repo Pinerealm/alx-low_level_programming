bcd0ffecd0ff0248ac13dc7bd2ad37786ced97f1
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-20 02:47:11 -0700

    C function: _strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr- finds first occurrence of char c in string s
+ *
+ * @s: pointer to string
+ * @c: source memory area
+ *
+ * Return: pointer to first occurrence of c 
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return ('\0');
+}
