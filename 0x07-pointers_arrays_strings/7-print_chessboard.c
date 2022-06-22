525bf3151ec163d626fb5e11ae9b031d3722f6dd
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-21 20:31:14 -0700

    Updated print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,13 +10,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	char *p = &(*a)[8];
+	int i, j;
 
-	for (i = 1; i <= 8; i++)
-		_putchar(*(p - i));
-	_putchar('\n');
-	for (i = 0; i <= 8; i++)
-		_putchar(*(p + i));
-	_putchar('\n');
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
 }
