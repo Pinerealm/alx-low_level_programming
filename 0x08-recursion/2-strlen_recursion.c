b4fb0dd3da7ce64a92fb7b694221f3d0ee452bf0
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-22 19:51:14 -0700

    C function: _strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to a string
+ *
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return(1 + _strlen_recursion(s + 1));
+}
