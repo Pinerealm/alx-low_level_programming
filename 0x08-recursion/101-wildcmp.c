0aae3a3ad3a21cc95c956988dcd151182aca1e2b
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-23 20:48:14 -0700

    C function: wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * wildcmp - checks if two strings are identical
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: 1 if identical, otherwise 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
