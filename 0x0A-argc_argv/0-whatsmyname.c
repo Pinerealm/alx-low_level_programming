5899ad14a120b8b1c205e6514d3c0155825d836c
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-24 21:46:52 -0700

    C program: print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+
+	return (0);
+}
