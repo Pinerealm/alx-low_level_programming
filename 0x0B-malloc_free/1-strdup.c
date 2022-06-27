ccc5b05344147c3d07d4bbe8e908c1d18126fe7a
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-27 11:32:41 -0700

    C function: _strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+int _strlen(char *s);
+
+/**
+ * _strdup - copies a string to a newly allocated space in memory
+ * @str: pointer to a string
+ *
+ * Return: pointer to newly created array
+ */
+
+char *_strdup(char *str)
+{
+	char *ar;
+	int i, len_s = _strlen(str);
+
+	if (!*str)
+		return ('\0');
+	ar = malloc(sizeof(char) * len_s + 1);
+	if (ar == NULL)
+		return ('\0');
+	for (i = 0; i < len_s; i++)
+	{
+		ar[i] = str[i];
+	}
+	ar[i] = '\0';
+	return (ar);
+}
+
+/**
+ * _strlen - finds the length of a string
+ * @s: pointer to a string
+ *
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int cnt = 0;
+
+	while (s[cnt] != '\0')
+		cnt++;
+
+	return (cnt);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,6 @@
 #define _MAIN_H
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
