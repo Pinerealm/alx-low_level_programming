bc523e2571a061f0986c51913d4aded15b0b5b5d
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-28 19:53:14 -0700

    C program: adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, res = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		res += atoi(argv[i]);
+	}
+	printf("%d\n", res);
+
+	return (0);
+}
