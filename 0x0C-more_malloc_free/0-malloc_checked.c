e02deba8780ee4b04ef7e14ed252fce6191c5881
Oluwaseyi Salami <osalami09@gmail.com>
2022-06-30 09:40:59 -0700

    C function: malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc and exits if fail
+ * @b: no of memory bytes
+ *
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (!p)
+		exit(98);
+	else
+		return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+void *malloc_checked(unsigned int b);
+
+#endif
