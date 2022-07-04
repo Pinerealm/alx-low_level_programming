c20b0d860d09581317d07da1903cae2af40cb490
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-04 03:57:02 -0700

    C program: prints the name of file an executable was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the file name it was compiled from
+ *
+ * Return: 0 if sucessful
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
