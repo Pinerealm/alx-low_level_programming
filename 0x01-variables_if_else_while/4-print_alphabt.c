983aed4525a1b2d50f64b2a3e5fba9dad9ea7386
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-05 20:52:42 -0700

    Removed unneeded headers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,7 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase then uppercase followed by a
- *        new line
+ * main - prints the alphabet in lowercase, except 'q'and 'e'
  *
  * Return: Always 0 (Success)
  */
