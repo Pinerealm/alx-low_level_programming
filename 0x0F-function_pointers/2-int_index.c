9cb71f42d0128f243ab805414c10e90b8f411ce8
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-06 19:54:05 -0700

    Added a conditional

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,11 +17,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
-			return (i);
-	}
+	if (array && cmp)
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 
 	return (-1);
 }
