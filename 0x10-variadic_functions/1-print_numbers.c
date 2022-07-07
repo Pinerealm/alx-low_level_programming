9768539abeb58645630c4b357366730ac4261011
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-07 16:11:19 -0700

    C function: prints numbers seperated by a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers seperated by a seperator
+ * @separator: character seperating numbers
+ * @n: number of parameters to sum up
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ints;
+	unsigned int i = 0;
+	int num;
+
+	if (!separator)
+		return;
+	va_start(ints, n);
+
+	for (num = va_arg(ints, int); i < n; num = va_arg(ints, int), i++)
+	{
+		if (i != (n - 1))
+			printf("%d%s", num, separator);
+		else
+			printf("%d", num);
+	}
+	putchar('\n');
+	va_end(ints);
+}
