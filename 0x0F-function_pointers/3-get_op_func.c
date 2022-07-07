2a3b8c8045f314fcb1a7528fed85b2dadb1772b4
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-06 18:29:24 -0700

    C program: perform arithmetic operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * chosen by a user
+ *
+ * @s: pointer to the operator
+ *
+ * Return: function pointer to the chosen operation
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while ((ops[i].op))
+	{
+		if (!strcmp(ops[i].op, s))
+			return (ops[i].f);
+		i++;
+	}
+
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts the second from the first integer
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Difference between the two integers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Multiplication product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides the first by the second integer
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Division result
+ */
+
+int op_div(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - performs a remainder division on two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Remainder of division operation
+ */
+
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
