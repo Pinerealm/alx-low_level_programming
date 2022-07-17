b2ad4dd7cd2c3c52d7d217f5bd772bb349d6e01e
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-17 15:18:39 -0700

    C function: free a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,8 +22,10 @@ typedef struct list_s
 
 int _putchar(char);
 size_t print_list(const list_t *);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+
+list_t *add_node_end(list_t **, const char *);
+void free_list(list_t *);
 
 #endif /* LISTS_H */
