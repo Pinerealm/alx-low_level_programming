0fce32b458673e4c497227aff667bdfa54c9c982
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-17 15:45:40 -0700

    Updated print_before_main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@
 void __attribute__((constructor)) print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my body for this great contest,\n");
+	printf("I bore my house upon my back!\n");
 }
