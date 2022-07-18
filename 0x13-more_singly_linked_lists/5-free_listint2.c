df2d78d395649952c105121ed2e0278fd0c7499b
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-18 13:19:12 -0700

    Another code change

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (head)
 	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
-	*head = NULL;
 }
