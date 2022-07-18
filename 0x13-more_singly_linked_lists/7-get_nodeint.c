2afd1684c68eec7bbfb881dfe52f475458005a0d
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-18 14:08:43 -0700

    C function: gets node data at a given index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - gets the node at a given index
+ *
+ * @head: address to the head pointer of the list
+ * @index: index of the node to get
+ *
+ * Return: address of the node at the given index, or NULL if not existing
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; head != NULL; i++)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+	}
+
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,6 +28,8 @@ listint_t *add_nodeint(listint_t **, const int);
 listint_t *add_nodeint_end(listint_t **, const int);
 void free_listint(listint_t *);
 void free_listint2(listint_t **);
-int pop_listint(listint_t **head);
+int pop_listint(listint_t **);
+
+listint_t *get_nodeint_at_index(listint_t *, unsigned int);
 
 #endif /* LISTS_H */
