5347c32eed65c1364878b31579ed628bbb9e8e66
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-21 06:30:03 -0700

    C function: convert binary string to unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary string to an unsigned int
+ * @b: pointer to the binary string
+ *
+ * Return: the converted unsigned int, or 0 if the string is not valid or NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, j = 32, k;
+
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	i = 0;
+	k = 0;
+	while (b[i])
+	{
+		if (j > 0)
+		{
+			k = k << 1;
+			if (b[i] == '1')
+				k = k | 1;
+			i++;
+			j--;
+		}
+	}
+	return (k);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+unsigned int binary_to_uint(const char *);
+
+#endif /* MAIN_H */
