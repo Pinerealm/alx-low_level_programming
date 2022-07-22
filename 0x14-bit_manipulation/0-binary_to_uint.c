6a6262d0826a1e015222f34b6203794df29b7147
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-21 17:41:24 -0700

    Removed whitespaces

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,10 +14,10 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 	for (i = 0; b[i]; i++)
-        {
-                if (b[i] != '0' && b[i] != '1')
-                        return (0);
-        }
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
 
 	for (--i; i >= 0; i--, place *= 2)
 	{
