d7b0edaef023171cf8bea0951790bd1f3e780c7e
Oluwaseyi Salami <osalami09@gmail.com>
2022-07-25 14:55:01 -0700

    Added null content condition

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,12 +20,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	len = _strlen(text_content);
-	b_write = write(fd, text_content, len);
-	if (b_write != len || b_write == -1)
-		return (-1);
+	if (text_content)
+	{
+		len = _strlen(text_content);
+		b_write = write(fd, text_content, len);
+		if (b_write == -1)
+			return (-1);
+	}
 	close(fd);
-
 	return (1);
 }
 
