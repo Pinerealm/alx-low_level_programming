71e35d9f4130566218d5c8aef0ccde699694333e
Pinerealm <osalami09@gmail.com>
2023-02-10 15:49:01 +0300

    Update function doc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print using printf
- *
- * Return: Always 0 (Success)
+ * main - Prints a string using printf
+ * 
+ * Description: This prints the string "with proper grammar, but the
+ * outcome is a piece of art,"
+ * Return: 0 (on success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
