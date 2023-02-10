4ed315a5d5d176b4a123c64074266fab60ff610d
Pinerealm <osalami09@gmail.com>
2023-02-10 17:21:16 +0300

    Change file descriptor to 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - print using write
+ * main - Print a string to standard error
  *
- * DESCRIPTION: print without using printf or puts
- *
- * Return: 1 (Success)
+ * Description: Do not use printf or puts or associated functions.
+ * The string to be printed is: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * followed by a new line.
+ * Return: 1
  */
-
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		60);
 	return (1);
 }
