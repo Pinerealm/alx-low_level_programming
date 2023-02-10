d548e56d12703c9629be91077e4abcececcf4707
Pinerealm <osalami09@gmail.com>
2023-02-10 16:09:17 +0300

    Remove trailing whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints a string using puts
- * 
+ *
  * Description: This prints the string '"Programming is like building a
  * multilingual puzzle'
  * Return: 0 (on success)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints a string using printf
- * 
+ *
  * Description: This prints the string "with proper grammar, but the
  * outcome is a piece of art,"
  * Return: 0 (on success)
