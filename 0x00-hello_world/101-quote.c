85e0199edcd727f811c5dba21ef44ac48472f0f9
Pinerealm <osalami09@gmail.com>
2023-02-10 17:38:18 +0300

    Break the string to be printed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
  * main - Print a string to standard error
  *
  * Description: Do not use printf or puts or associated functions.
- * The string to be printed is: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Print the string 'and that piece of art is useful"- Dora Korpar, 2015-10-19'
  * followed by a new line.
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		60);
+	write(2, "and that piece of art is useful\" - Dora Korpar,"
+		 " 2015-10-19\n", 59);
 	return (1);
 }
