b1d7da7079b30b5fe9a945f688c7a05e2c56521f
Pinerealm <osalami09@gmail.com>
2023-02-10 16:09:58 +0300

    Change format specifier to %lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print sizes of different data types
+ * main - Prints the size of various types on the computer it is run on
  *
- * Return: Always 0 (Success)
+ * Return: 0 (on success)
  */
-
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s),\n", sizeof(char));
+	printf("Size of an int: %lu byte(s),\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s),\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s),\n",
+		sizeof(long long int));
+	printf("Size of a float: %lu byte(s),\n", sizeof(float));
+
 	return (0);
 }
