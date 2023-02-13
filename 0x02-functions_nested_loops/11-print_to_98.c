cadcccd1522673c2a5a273fdad4c336cb68c3543
Pinerealm <osalami09@gmail.com>
2023-02-13 21:03:50 +0300

    Change variable value within printf

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,13 +13,11 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%d, ", n);
-		n++;
+		printf("%d, ", n++);
 	}
 	while (n > 98)
 	{
-		printf("%d, ", n);
-		n--;
+		printf("%d, ", n--);
 	}
 	if (n == 98)
 	{
