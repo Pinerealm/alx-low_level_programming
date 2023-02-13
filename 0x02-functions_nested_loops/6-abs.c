c58c44c2d4f3e41e1befc7fbd89c8d141426e87b
Pinerealm <osalami09@gmail.com>
2023-02-13 17:49:49 +0300

    Use just one return statement

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * _abs - function that computes the absolute value of an integer
- * @n: character to be tested
+ * _abs - computes the absolute value of an integer
+ * @n: number to compute
  *
  * Return: absolute value of n
  */
-
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		return (n * -1);
+		n = n * -1;
 	}
-	else
-	{
-		return (n);
-	}
+	return (n);
 }
