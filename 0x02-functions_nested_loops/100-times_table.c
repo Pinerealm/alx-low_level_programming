7e04b1390f6434c6aa9f79da5f15feec19e6b506
Pinerealm <osalami09@gmail.com>
2023-02-13 20:56:42 +0300

    Implement another solution

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,73 @@
 #include "main.h"
 
+void print_product(int a, int b);
 /**
- * print_times_table - prints the given n times table
+ * print_times_table - prints the n times table, starting with 0
+ * @n: starting number
  *
- * @n: integer of the times table
- *
- * Return: void
+ * Condition: if n is greater than 15 or less than 0, the function will
+ * not print anything
  */
-
 void print_times_table(int n)
 {
-	int a = 0;
-	int b, res;
+	int i = 0, j;
 
-	if (n < 0 || n > 15)
+	if (n > 15 || n < 0)
 		return;
-	while (a <= n)
+
+	while (i <= n)
 	{
-		for (b = 0; b <= n; b++)
+		j = 0;
+		while (j <= n)
 		{
-			res = a * b;
-			if (b == 0)
-				_putchar(res + '0');
-			else if (res < 10)
+			print_product(i, j);
+			if (j == n)
+				break;
+			if (i * (j + 1) < 10)
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(res + '0');
+				_putchar(' ');
 			}
-			else if (res < 100)
+			else if (i * (j + 1) < 100)
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(res / 10 + '0');
-				_putchar(res % 10 + '0');
+				_putchar(' ');
 			}
 			else
-			{
-				_putchar(res / 100 + '0');
-				_putchar((res - 100) / 10 + '0');
-				_putchar(res % 10 + '0');
-			}
-			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+			j++;
 		}
+		i++;
 		_putchar('\n');
-		a++;
+	}
+}
+
+/**
+ * print_product - print the products of the multiplication table
+ * @a: The first integer
+ * @b: The second integer
+ */
+void print_product(int a, int b)
+{
+	int product = a * b;
+
+	if (product < 10)
+		_putchar(product + '0');
+	else if (product < 100)
+	{
+		_putchar(product / 10 + '0');
+		_putchar(product % 10 + '0');
+	}
+	else
+	{
+		_putchar(product / 100 + '0');
+		_putchar((product / 10) % 10 + '0');
+		_putchar(product % 10 + '0');
 	}
 }
