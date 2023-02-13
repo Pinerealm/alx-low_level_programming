6de702dfee7514cef8fd4e16b078ae4f3b8a8755
Pinerealm <osalami09@gmail.com>
2023-02-13 18:55:53 +0300

    Change for to while loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * jack_bauer - function that prints every minute of the day starting from
- *              00:00 to 23:59
- *
- * Return: value of the last digit of n
+ * jack_bauer - prints every minute of the day of Jack Bauer starting from
+ * 00:00 to 23:59
  */
-
 void jack_bauer(void)
 {
-	int h, m;
+	int h = 0, m;
 
-	for (h = 0; h < 24; h++)
+	while (h < 24)
 	{
 		m = 0;
 		while (m < 60)
@@ -19,12 +16,11 @@ void jack_bauer(void)
 			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
 			_putchar(':');
-
 			_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
 			_putchar('\n');
-
 			m++;
 		}
+		h++;
 	}
 }
