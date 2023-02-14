3fedcf8f2ecd0bdb61535311abd52b0ee1b90a77
Pinerealm <osalami09@gmail.com>
2023-02-15 01:53:55 +0300

    Implement fxn with a while loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 - 9 followed by a new line
- *
- * Return: void
+ * print_numbers - prints the numbers, from 0 to 9
  */
-
 void print_numbers(void)
 {
-	int i;
+	char c = '0';
 
-	for (i = 48; i <= 57; i++)
+	while (c <= '9')
 	{
-		_putchar(i);
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
