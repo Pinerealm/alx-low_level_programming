56c72a0e34f436db9f4d55a9219d54dbec7bbf3b
Pinerealm <osalami09@gmail.com>
2023-02-14 05:28:42 +0300

    Store extra numbers using modulo arithmetic

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 98 Fibonacci numbers starting with 1
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: finds and prints the first 98 Fibonacci numbers
+ * starting with 1 and 2. Use of long long isn't allowed
+ * Return: 0 (on success)
  */
-
 int main(void)
 {
-	long i = 1;
-	long j = 2;
-	long sum, n;
+	unsigned long i = 1, j = 2, i1, j1, sum, extra;
+	short count = 0;
 
-	printf("%lu, ", i);
-	printf("%lu, ", j);
-
-	for (n = 0; n < 96; n++)
+	printf("%lu, %lu, ", i, j);
+	while (count < 90)
 	{
 		sum = i + j;
-		if (n == 95)
-			printf("%lu", sum);
-		else
-			printf("%lu, ", sum);
+		printf("%lu, ", sum);
 		i = j;
 		j = sum;
+		count++;
+	}
+	i1 = i % 1000000000;
+	j1 = j % 1000000000;
+	i = i / 1000000000;
+	j = j / 1000000000;
+
+	while (count < 96)
+	{
+		sum = i + j;
+		extra = i1 + j1;
+		printf("%lu%lu, ", sum, extra);
+		i = j;
+		i1 = j1;
+		j = sum;
+		j1 = extra;
+
+		count++;
 	}
 	printf("\n");
-
 	return (0);
 }
