5171d6196e7ef22aa680b6e2108a7302fba1e02b
Pinerealm <osalami09@gmail.com>
2023-02-15 03:30:03 +0300

    Implement solution using a while loop

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,30 +1,26 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line in the terminal
- * @n: number of times to draw line
- *
- * Return: void
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
-
 void print_diagonal(int n)
 {
-	int i, space;
+	int i = 0, j = 0;
+
+	while (i < n)
+	{
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		j = 0;
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			for (space = 0; space < i; space++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
 }
