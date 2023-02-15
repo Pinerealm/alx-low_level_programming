10cc03f88bdcdf0ff7aef9103ba902c0b1128930
Pinerealm <osalami09@gmail.com>
2023-02-15 05:27:17 +0300

    Implement a non-recursive solution

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,35 @@
 #include "main.h"
 
 /**
-* print_number - Prints a number
-* @n: The number to print
-*
-* Return: void
-*/
-
+ * print_number - prints an integer
+ * @n: the integer to print
+ */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int num, divisor = 1, digits = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n = -n;
 	}
-	if (num > 9)
+	/* Get the no of digits in n */
+	num = n;
+	while (num > 0)
 	{
-		print_number(num / 10);
+		num /= 10;
+		digits++;
 	}
-	_putchar(num % 10 + '0');
+	/* Get the divisor */
+	while (digits > 1)
+	{
+		divisor *= 10;
+		digits--;
+	}
+	/* Print the number */
+	while (divisor > 0)
+	{
+		_putchar((n / divisor) % 10 + '0');
+		divisor /= 10;
+	}
 }
