418b4813c57d1f513ac2bb8f3cfe47a4675ddfea
Pinerealm <osalami09@gmail.com>
2023-02-20 02:04:28 +0300

    Implement another solution

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,64 @@
 #include "main.h"
+int _isdigit(char c);
 
 /**
- * _atoi - converts a string to an integer
- * @s: string containing a number
+ * _atoi - convert a string to an integer
+ * @s: string to convert
  *
- * Return: value of integer
+ * Return: integer value of the string
  */
-
 int _atoi(char *s)
 {
-	int i, j, k, len, m, tmp;
+	int res, idx, num_idx, num_len, sign;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	len = 0;
-	m = 0;
-	tmp = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && m == 0)
+	res = idx = num_len = sign = 0;
+	while (s[idx] != '\0')
 	{
-		if (s[i] == '-')
-			++j;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[idx] == '-')
 		{
-			tmp = s[i] - '0';
-			if (j % 2)
-				tmp = -tmp;
-			k = k * 10 + tmp;
-			m = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			m = 0;
+			sign++;
+			idx++;
+			continue;
 		}
-		i++;
+		else if (s[idx] == '+')
+		{
+			sign--;
+			idx++;
+			continue;
+		}
+		if (_isdigit(s[idx]))
+		{
+			num_idx = idx;
+			while (_isdigit(s[idx]))
+			{
+				idx++;
+			}
+			num_len = idx - num_idx;
+		}
+		if (num_len > 0)
+			break;
+		idx++;
 	}
+	idx = num_idx;
+	while (num_len > 0)
+	{
+		res = res * 10 + (s[idx] - '0');
+		num_len--;
+		idx++;
+	}
+	if (sign > 0 && res > 0)
+		res *= -1;
 
-	if (m == 0)
-		return (0);
+	return (res);
+}
 
-	return (k);
+/**
+ * _isdigit - check if a character is a digit
+ * @c: character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
 }
