0d159c34f3423eec77d56f64f905d87ed96a6f40
Pinerealm <osalami09@gmail.com>
2023-02-21 23:42:38 +0300

    Ensure a total of n bytes are written

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[idx] = src[idx];
 		idx++;
 	}
-	dest[idx] = '\0';
+	while (idx < n)
+	{
+		dest[idx] = '\0';
+		idx++;
+	}
 
 	return (dest);
 }
