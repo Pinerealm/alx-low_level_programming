6e0b866a28997f05ecf7a27cfebbd7a02a4f9945
Pinerealm <osalami09@gmail.com>
2023-02-22 00:25:24 +0300

    Change the while loop 'and' condition to 'or'

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int idx = 0;
 
-	while (s1[idx] != '\0' && s2[idx] != '\0')
+	while (s1[idx] != '\0' || s2[idx] != '\0')
 	{
 		if (s1[idx] != s2[idx])
 			return (s1[idx] - s2[idx]);
 		idx++;
 	}
+
 	return (0);
 }
