c181e5d9019994374187e462d4c10695f9fcfcb0
Pinerealm <osalami09@gmail.com>
2023-02-22 03:02:38 +0300

    Use string quotes to initialize arrays

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,44 +2,27 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @s: pointer to a string
+ * @s: string to modify
  *
- * Return: s
+ * Return: pointer to the resulting string
  */
-
 char *rot13(char *s)
 {
-	int i = 0, j;
-	char alphab_lu[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-			'G', 'H', 'I', 'J', 'K', 'L',
-			'M', 'N', 'O', 'P', 'Q', 'R',
-			'S', 'T', 'U', 'V', 'W', 'X',
-			'Y', 'Z', 'a', 'b', 'c', 'd',
-			'e', 'f', 'g', 'h', 'i', 'j',
-			'k', 'l', 'm', 'n', 'o', 'p',
-			'q', 'r', 's', 't', 'u', 'v',
-			'w', 'x', 'y', 'z'};
-	char cipher[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-			'T', 'U', 'V', 'W', 'X', 'Y',
-			'Z', 'A', 'B', 'C', 'D', 'E',
-			'F', 'G', 'H', 'I', 'J', 'K',
-			'L', 'M', 'n', 'o', 'p', 'q',
-			'r', 's', 't', 'u', 'v', 'w',
-			'x', 'y', 'z', 'a', 'b', 'c',
-			'd', 'e', 'f', 'g', 'h', 'i',
-			'j', 'k', 'l', 'm'};
+	int idx = 0, sdx;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[i])
+	while (s[idx] != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		sdx = 0;
+		while (alpha[sdx] != '\0')
 		{
-			if (s[i] == alphab_lu[j])
-			{
-				s[i] = cipher[j];
-				break;
-			}
+			if (s[idx] == alpha[sdx])
+				s[idx] = rot13[sdx];
+			sdx++;
 		}
-		i++;
+		idx++;
 	}
+
 	return (s);
 }
