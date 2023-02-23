eb6529fbac9f45a3c3560f1d80ce066c9bad0ba7
Pinerealm <osalami09@gmail.com>
2023-02-23 23:15:43 +0300

    Implement another solution

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,20 +2,18 @@
 
 /**
  * print_chessboard - prints the chessboard
- *
- * @a: array containing character
- *
- * Return: void
+ * @a: array of 8 strings
  */
-
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int row, col;
 
-	for (i = 0; a[i][7]; i++)
+	for (row = 0; row < 8; row++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
+		for (col = 0; col < 8; col++)
+		{
+			_putchar(a[row][col]);
+		}
 		_putchar('\n');
 	}
 }
