82fd01e8748df80e9c3f3eb960fcdf4df92f8daf
Pinerealm <osalami09@gmail.com>
2023-02-24 20:55:34 +0300

    Modify second if statement

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a number n
- * @n: a number
+ * factorial - finds the factorial of a number
+ * @n: number
  *
- * Return: factorial n
+ * Return: factorial of n
  */
-
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	if (n <= 1)
-	{
+	if (n == 0)
 		return (1);
-	}
 	return (n * factorial(n - 1));
 }
