4425601f12b4e324a3792550c14af3bdaebd8487
Pinerealm <osalami09@gmail.com>
2023-02-24 22:04:30 +0300

    Remove condition testing if n is less than 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,29 @@
 #include "main.h"
-
-int _sqrt(int n, int i);
+int sqrt_helper(int n, int i);
 
 /**
- * _sqrt_recursion - apply the _sqrt function with i equals 1
+ * _sqrt_recursion - finds the natural square root of a number
  * @n: number
  *
- * Return: natural square root of n, -1 if not natural
+ * Return: natural square root of n or -1 if n has no natural square
  */
-
 int _sqrt_recursion(int n)
 {
-	return (_sqrt(n, 1));
+	return (sqrt_helper(n, 1));
 }
 
 /**
- * _sqrt - calculates the natural square root of a number
- * @n: number to calculate from
- * @i: test no
+ * sqrt_helper - finds the natural square root of a number
+ * @n: number
+ * @i: test number
  *
- * Return: natural square root of n, -1 if not natural
+ * Return: natural square root of n or -1 if n has no natural square
  */
-
-int _sqrt(int n, int i)
+int sqrt_helper(int n, int i)
 {
-	if (n < 0)
-		return (-1);
-	if ((i * i) > n)
-		return (-1);
 	if (i * i == n)
 		return (i);
-	return (_sqrt(n, ++i));
+	if (i * i > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
 }
