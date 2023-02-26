f8359d94ee126dccf95dd9b396f9cc8b14b318ab
Pinerealm <osalami09@gmail.com>
2023-02-27 02:19:58 +0300

    Implement another solution

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,25 @@
 #include "main.h"
 
 /**
- * wildcmp - checks if two strings are identical
- * @s1: pointer to first string
- * @s2: pointer to second string
+ * wildcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
  *
- * Return: 1 if identical, otherwise 0
+ * Return: 1, if strings are identical, 0 otherwise
  */
-
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s2 == '\0');
-	}
-
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1, s2 + 1))
+			return (1);
+		return (wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
