01e40faaa51ba5ddc05ff7ccc1fe65f160c7629d
Pinerealm <osalami09@gmail.com>
2023-02-27 16:44:30 +0300

    Add a ranlib command line

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
-ar -rc liball.a *.o
+# Create a static library called liball.a from all the .c files 
+# that are in the current directory.
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c ./*.c
+ar rc liball.a ./*.o
+ranlib liball.a
