f3810286e8e204f81684c4cc7b149570f972c6ca
Pinerealm <osalami09@gmail.com>
2023-03-18 16:46:30 +0300

    Display a given amount of main fxn opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Description: print the opcodes of own main function
+ * Return: 0 (on success)
+ */
+int main(int argc, char *argv[])
+{
+	int idx, n;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	
+	for (idx = 0; idx < n; idx++)
+	{
+		printf("%02hhx", ((char *)main)[idx]);
+		if (idx < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
