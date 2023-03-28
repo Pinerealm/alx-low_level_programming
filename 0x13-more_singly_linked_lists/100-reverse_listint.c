d44f30bee21c3a2a7d733fd59e3abde8cbd21b35
Pinerealm <osalami09@gmail.com>
2023-03-28 23:04:38 +0300

    Make minor changes to the reverse_listintint fxn

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,14 +2,13 @@
 
 /**
  * reverse_listint - reverses a linked listint_t list
- * @head: address of the pointer to the list head
+ * @head: double pointer to the list head
  *
  * Return: address of the new head of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next;
+	listint_t *prev = NULL, *next;
 
 	while (*head)
 	{
@@ -19,5 +18,6 @@ listint_t *reverse_listint(listint_t **head)
 		*head = next;
 	}
 	*head = prev;
+
 	return (*head);
 }
