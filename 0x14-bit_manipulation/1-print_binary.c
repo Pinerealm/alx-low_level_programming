6c6c6e8705393f4fe72793ffc8194593e24c0459
Pinerealm <osalami09@gmail.com>
2023-04-04 09:35:53 +0300

    Refactor the print_binary function

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,20 +2,11 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: number to print
- *
- * Return: void
+ * @n: number to be printed
  */
 void print_binary(unsigned long int n)
 {
-	if (n)
-	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
-	{
-		_putchar('0');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
