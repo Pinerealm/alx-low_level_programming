af2c87a4e2285feb33c8e9ff83c8522b3dd8755d
Pinerealm <osalami09@gmail.com>
2023-04-06 08:33:29 +0300

    Refactor the create_file fxn

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,29 @@
 #include "main.h"
 
-int _strlen(char *str);
-
 /**
  * create_file - creates a file
- *
  * @filename: name of the file to create
- * @text_content: content of the file
+ * @text_content: NULL terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int n;
+	int fd, len = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	n = write(fd, text_content, _strlen(text_content));
-	if (n == -1)
-		return (-1);
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		if (write(fd, text_content, len) == -1)
+			return (-1);
+	}
+
 	close(fd);
-
 	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @str: pointer to the string
- *
- * Return: length of the string
- */
-int _strlen(char *str)
-{
-	int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
 }
