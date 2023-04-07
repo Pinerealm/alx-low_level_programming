4ea8a11fdb1bc150447f97e8c192b72e1915b34c
Pinerealm <osalami09@gmail.com>
2023-04-07 20:03:10 +0300

    Add another check for malloc failure

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -40,6 +40,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
