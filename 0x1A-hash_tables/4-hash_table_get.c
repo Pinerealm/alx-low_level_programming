67cf0cb810b0101668dd673ba342fc776f4925a7
Pinerealm <osalami09@gmail.com>
2023-04-07 20:38:24 +0300

    Return NULL if an empty string was passed

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 	unsigned long index;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
