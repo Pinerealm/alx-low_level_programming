0849e77fde09e0be6563c97b44281123bc360273
Pinerealm <osalami09@gmail.com>
2025-11-08 14:13:41 +0300

    Refactor digit combination logic to simplify conditions and improve readability

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,20 +12,17 @@ int main(void)
 	int num1, num2;
 
 	num1 = '0';
-	while (num1 <= '9')
+	while (num1 <= '8')
 	{
-		num2 = '1';
+		num2 = num1 + 1;
 		while (num2 <= '9')
 		{
-			if (num1 < num2)
+			putchar(num1);
+			putchar(num2);
+			if (num1 != '8' || num2 != '9')
 			{
-				putchar(num1);
-				putchar(num2);
-				if (!(num1 == '8' && num2 == '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 			num2++;
 		}
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,25 +12,22 @@ int main(void)
 	int num1, num2, num3;
 
 	num1 = '0';
-	while (num1 <= '9')
+	while (num1 <= '7')
 	{
-		num2 = '1';
-		while (num2 <= '9')
+		num2 = num1 + 1;
+		while (num2 <= '8')
 		{
-			num3 = '2';
+			num3 = num2 + 1;
 			while (num3 <= '9')
 			{
-				if (num1 < num2 && num2 < num3)
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if (num1 != '7' || num2 != '8'
+					|| num3 != '9')
 				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
-					if (!(num1 == '7' && num2 == '8'
-						&& num3 == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 				num3++;
 			}
