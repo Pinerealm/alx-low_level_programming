57c11c82b6dcca4e34e7c201876648eed1d80bf7
Pinerealm <osalami09@gmail.com>
2025-11-08 23:01:25 +0300

    Refactor Fibonacci calculation to use separate high and low parts for large numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,43 +9,33 @@
  */
 int main(void)
 {
-	unsigned long i = 1, j = 2, i1, j1, sum, extra;
-	unsigned long billion = 1000000000;
-	short count = 0;
+	unsigned long fib1_low = 1, fib2_low = 2;
+	unsigned long fib1_high = 0, fib2_high = 0;
+	unsigned long sum_low, sum_high, carry;
+	unsigned long divisor = 1000000000; /* 10^9 */
+	int count;
 
-	printf("%lu, %lu, ", i, j);
-	while (count < 90)
-	{
-		sum = i + j;
-		printf("%lu, ", sum);
-		i = j;
-		j = sum;
-		count++;
-	}
-	i1 = i % billion;
-	j1 = j % billion;
-	i = i / billion;
-	j = j / billion;
+	printf("%lu", fib1_low);
 
-	while (count < 96)
+	for (count = 2; count <= 98; count++)
 	{
-		sum = i + j;
-		extra = i1 + j1;
-		if (extra > billion)
-		{
-			sum += extra / billion;
-			extra = extra % billion;
-		}
-		if (count == 95)
-			printf("%lu%lu", sum, extra);
+		sum_low = fib1_low + fib2_low;
+		carry = sum_low / divisor;
+		sum_low = sum_low % divisor;
+
+		sum_high = fib1_high + fib2_high + carry;
+
+		if (sum_high > 0)
+			printf(", %lu%09lu", sum_high, sum_low);
 		else
-			printf("%lu%lu, ", sum, extra);
-		i = j;
-		i1 = j1;
-		j = sum;
-		j1 = extra;
-		count++;
+			printf(", %lu", sum_low);
+
+		fib1_low = fib2_low;
+		fib1_high = fib2_high;
+		fib2_low = sum_low;
+		fib2_high = sum_high;
 	}
+
 	printf("\n");
 	return (0);
 }
