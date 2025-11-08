f5e975c507d0607742cac3890f6ebc109d4444fe
Pinerealm <osalami09@gmail.com>
2025-11-08 14:22:46 +0300

    Refactor combination printing logic to eliminate unnecessary conditions and improve clarity

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,24 +12,21 @@ int main(void)
 	int num1, num2;
 
 	num1 = 0;
-	while (num1 <= 99)
+	while (num1 <= 98)
 	{
 		num2 = num1 + 1;
 		while (num2 <= 99)
 		{
-			if (num1 < num2)
-			{
-				putchar(num1 / 10 + '0');
-				putchar(num1 % 10 + '0');
-				putchar(' ');
-				putchar(num2 / 10 + '0');
-				putchar(num2 % 10 + '0');
+			putchar(num1 / 10 + '0');
+			putchar(num1 % 10 + '0');
+			putchar(' ');
+			putchar(num2 / 10 + '0');
+			putchar(num2 % 10 + '0');
 
-				if (!(num1 == 98 && num2 == 99))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 			num2++;
 		}
