5421b0af521d5f9d45d51e306ba8569c42546603
Pinerealm <osalami09@gmail.com>
2025-11-11 14:06:52 +0300

    Refactor _atoi function to simplify logic and remove unnecessary variables

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,3 @@
-#include "main.h"
-int _isdigit(char c);
-
 /**
  * _atoi - convert a string to an integer
  * @s: string to convert
@@ -9,44 +6,22 @@ int _isdigit(char c);
  */
 int _atoi(char *s)
 {
-	int res, idx, num_idx, num_len, sign, tmp;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-	res = idx = num_len = sign = 0;
-	while (s[idx] != '\0')
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
-		if (s[idx] == '-')
-		{
-			sign++;
-			idx++;
-			continue;
-		}
-		if (_isdigit(s[idx]))
-		{
-			num_idx = idx;
-			while (_isdigit(s[idx]))
-			{
-				tmp = s[idx] - '0';
-				if (sign % 2)
-					tmp = -tmp;
-				res = res * 10 + tmp;
-				idx++;
-			}
-			num_len = idx - num_idx;
-		}
-		if (num_len > 0)
-			break;
-		idx++;
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
 	}
-	return (res);
-}
+	
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-/**
- * _isdigit - check if a character is a digit
- * @c: character to check
- *
- * Return: 1 if digit, 0 otherwise
- */
-int _isdigit(char c)
-{
-	return (c >= '0' && c <= '9');
+	return (result * sign);
 }
