55842cba623a882a526531a1c344e329b1f30c27
Pinerealm <osalami09@gmail.com>
2025-11-12 13:36:16 +0300

    Refactor _strcmp function to simplify string comparison logic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * _strcmp - compare two strings
  * @s1: first string
@@ -11,12 +9,8 @@ int _strcmp(char *s1, char *s2)
 {
 	int idx = 0;
 
-	while (s1[idx] != '\0' || s2[idx] != '\0')
-	{
-		if (s1[idx] != s2[idx])
-			return (s1[idx] - s2[idx]);
+	while (s1[idx] && s1[idx] == s2[idx])
 		idx++;
-	}
 
-	return (0);
+	return (s1[idx] - s2[idx]);
 }
