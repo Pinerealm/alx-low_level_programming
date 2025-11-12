082e12573acceb491aca1bd2e1609a45ea383f7a
Pinerealm <osalami09@gmail.com>
2025-11-12 09:42:32 +0300

    Change result variable type to unsigned int in _atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,7 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
+	unsigned int result = 0;
 	int sign = 1;
 	int i = 0;
 
@@ -16,7 +16,7 @@ int _atoi(char *s)
 			sign *= -1;
 		i++;
 	}
-	
+
 	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
 	{
 		result = result * 10 + (s[i] - '0');
