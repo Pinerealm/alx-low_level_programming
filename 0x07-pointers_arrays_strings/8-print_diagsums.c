175746a159dbb3bb8ee971326d06606591b0352b
Pinerealm <osalami09@gmail.com>
2025-11-13 19:28:22 +0300

    Refactor print_diagsums function to simplify diagonal sum calculation and improve readability

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,25 +8,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum = 0;
+	int i, main_diag_sum = 0, anti_diag_sum = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				sum += *(a + (i * size) + j);
-		}
+		main_diag_sum += *(a + i * size + i);
+		anti_diag_sum += *(a + i * size + (size - 1 - i));
 	}
-	printf("%d, ", sum);
-	sum = 0;
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i + j == size - 1)
-				sum += *(a + (i * size) + j);
-		}
-	}
-	printf("%d\n", sum);
+
+	printf("%d, %d\n", main_diag_sum, anti_diag_sum);
 }
