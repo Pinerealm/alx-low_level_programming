47559246df3e028d6d4b417d91473da89341573b
Pinerealm <osalami09@gmail.com>
2025-11-14 20:52:57 +0300

    Refactor wildcmp function for improved logic and readability

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,17 +9,28 @@
  */
 int wildcmp(char *s1, char *s2)
 {
+	/* Both strings exhausted */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+
+	/* Skip consecutive '*' in s2 */
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '*')
-			return (wildcmp(s1, s2 + 1));
-		if (wildcmp(s1, s2 + 1))
+		while (*(s2 + 1) == '*')
+			s2++;
+
+		/* Wildcard at the end of s2 matches remaining s1 */
+		if (*(s2 + 1) == '\0')
 			return (1);
-		return (wildcmp(s1 + 1, s2));
+
+		/* Try to match the rest of s2 */
+		return (wildcmp(s1, s2 + 1)
+			|| (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
+
+	/* Characters match, advance both */
+	if (*s1 == *s2 && *s1 != '\0')
+		return (wildcmp(s1 + 1, s2 + 1));
+
 	return (0);
 }
