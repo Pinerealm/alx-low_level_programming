687ac6c2ef02b1db96b085180e1c6ebf8afcbb8a
Pinerealm <osalami09@gmail.com>
2025-11-16 20:42:14 +0300

    Refactor print_strings function to improve separator handling

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,12 +19,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	str = va_arg(strs, char *);
 	for ( ; i < n; str = va_arg(strs, char *), i++)
 	{
+		if (i > 0 && separator)
+			printf("%s", separator);
 		if (str)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		if (separator && i < (n - 1))
-			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(strs);
