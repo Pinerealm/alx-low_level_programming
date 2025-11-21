5318fefa84eb3b67d1786bede7730d54d7259233
Pinerealm <osalami09@gmail.com>
2025-11-21 19:49:23 +0300

    Refactor add_node function to simplify node addition logic

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,12 +17,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = _strlen(str);
-	new->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
 	new->next = *head;
 	*head = new;
 
