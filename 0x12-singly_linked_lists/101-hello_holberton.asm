432b7a9dc833cbc70b2625646a88ea190c94c431
Pinerealm <osalami09@gmail.com>
2025-11-21 19:51:16 +0300

    Add hello_holberton.asm to print "Hello, ALX"

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    msg db "Hello, ALX", 10, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rdi, msg
+    mov rax, 0
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    mov rax, 0
+    ret
