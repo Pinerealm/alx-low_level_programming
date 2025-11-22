bf172f283a63281a41a63c6f0812c97ece473dab
Pinerealm <osalami09@gmail.com>
2025-11-22 18:37:12 +0300

    Refactor print_listint_safe to simplify loop and remove unnecessary array

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,31 +8,24 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t l_idx = 0, ptr_idx, loop = 0;
-	const listint_t *tmp = head, *ptr_arr[1024];
+	size_t l_idx = 0;
+	const listint_t *tmp = head, *prev;
 
 	if (head == NULL)
 		return (0);
 	while (tmp)
 	{
-		if (l_idx >= 1024)
-			exit(98);
-		ptr_arr[l_idx] = tmp;
 		printf("[%p] %d\n", (void *)tmp, tmp->n);
+		prev = tmp;
 		tmp = tmp->next;
 		l_idx++;
 
-		for (ptr_idx = 0; ptr_idx < l_idx; ptr_idx++)
+		if (prev <= tmp || tmp == head)
 		{
-			if (tmp == ptr_arr[ptr_idx])
-			{
-				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
-				loop = 1;
-				break;
-			}
-		}
-		if (loop)
+			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
 			break;
+		}
+
 	}
 	return (l_idx);
 }
