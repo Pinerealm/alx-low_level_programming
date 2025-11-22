96e57fe203b9016a85af72391fa0d350d5b8b934
Pinerealm <osalami09@gmail.com>
2025-11-22 18:42:00 +0300

    Revert to previous solution

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,24 +8,28 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t l_idx = 0;
-	const listint_t *tmp = head, *prev;
+	size_t l_idx = 0, ptr_idx;
+	const listint_t *tmp = head, *ptr_arr[1024];
 
 	if (head == NULL)
 		return (0);
 	while (tmp)
 	{
+		if (l_idx >= 1024)
+			exit(98);
+		ptr_arr[l_idx] = tmp;
 		printf("[%p] %d\n", (void *)tmp, tmp->n);
-		prev = tmp;
 		tmp = tmp->next;
 		l_idx++;
 
-		if (prev <= tmp || tmp == head)
+		for (ptr_idx = 0; ptr_idx < l_idx; ptr_idx++)
 		{
-			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
-			break;
+			if (tmp == ptr_arr[ptr_idx])
+			{
+				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+				return (l_idx);
+			}
 		}
-
 	}
 	return (l_idx);
 }
