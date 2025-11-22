12d9b77f6fb3efcd9b8d0f69832930e3ceb30b53
Pinerealm <osalami09@gmail.com>
2025-11-22 21:02:08 +0300

    Fix bit manipulation functions to use correct comparison and type for bitwise operations

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,8 +10,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > (sizeof(unsigned long int) * 8))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n |= (1 << index);
+	*n |= (1UL << index);
 	return (1);
 }
