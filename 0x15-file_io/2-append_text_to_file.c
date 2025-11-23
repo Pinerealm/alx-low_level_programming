51b07e3648be2a3eec90b6b0b627ee7785792738
Pinerealm <osalami09@gmail.com>
2025-11-23 05:01:01 +0300

    Remove whitespace

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (write(fd, text_content, len) == -1)
 			w_status = -1;
 	}
-	
+
 	if (close(fd) == -1)
 		return (-1);
 	return (w_status);
