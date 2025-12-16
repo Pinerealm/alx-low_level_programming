f744c19ac032a38ee471644907fc7412f3582152
Pinerealm <osalami09@gmail.com>
2025-12-16 21:34:51 +0300

    Add shebang line again

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 gcc -shared -o /tmp/libwin.so -fPIC -xc - <<< 'int rand(){static int i=0;int n[]={8,7,9,23,74,8};return n[i++%6];}void srand(unsigned int s){}'
 export LD_PRELOAD=/tmp/libwin.so
