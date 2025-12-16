1366a4d41b29a0a3e86082ee1d5c3ab122998c77
Pinerealm <osalami09@gmail.com>
2025-12-16 22:06:45 +0300

    Fix random number generation

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o /tmp/libwin.so -fPIC -xc - <<< "$(printf "int rand(){static int i=0\073int n[]={8,7,9,23,74,8}\073return n[i++%%6]\073}void srand(unsigned int s){}")"
+gcc -shared -o /tmp/libwin.so -fPIC -xc - <<< "$(printf "int rand(){static int i=0\073int n[]={8,8,7,9,23,74}\073return n[i++%%6]\073}void srand(unsigned int s){}")"
 export LD_PRELOAD=/tmp/libwin.so
