924cd4a8ba03859530d7130dad0a143006913d3e
Pinerealm <osalami09@gmail.com>
2025-12-21 05:59:35 +0300

    Refactor hash_table_set to use replace_value for updating existing keys

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+int replace_value(hash_node_t *node, const char *key, const char *value);
 
 /**
  * hash_table_set - adds an element to a hash table
@@ -10,25 +11,14 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *tmp;
+	hash_node_t *new_node;
 	unsigned long int index;
 
 	if (!ht || !key || !value || *key == '\0')
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			free(tmp->value);
-			tmp->value = strdup(value);
-			if (tmp->value == NULL)
-				return (0);
-			return (1);
-		}
-		tmp = tmp->next;
-	}
+	if ((replace_value(ht->array[index], key, value)) == 1)
+		return (1);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
@@ -50,4 +40,33 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = new_node;
 
 	return (1);
+}
+
+/**
+ * replace_value - Replaces the value associated with a given key in a
+ *                 hash node list.
+ * @node: Pointer to the head of the hash node linked list.
+ * @key: The key whose value should be replaced.
+ * @value: The new value to associate with the key.
+ *
+ * Return: 1 if the value was successfully replaced, 0 otherwise.
+ */
+int replace_value(hash_node_t *node, const char *key, const char *value)
+{
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			char *new_value = strdup(value);
+
+			if (!new_value)
+				return (0);
+			free(node->value);
+			node->value = new_value;
+			return (1);
+		}
+		node = node->next;
+	}
+
+	return (0);
 }
